d556b9f433f0fce034d3a8bc2bbd0f00b1ba4158
albireo145 <34166466+albireo145@users.noreply.github.com>
2017-12-09 18:35:35 +0900

    169

---
+++ LeetCodeInJs/169_MajorityElement.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    var major = 0;
+    var count = 0;
+    for(num of nums){
+        if(count == 0){
+            major = num;
+            count++;
+        }else if(count==num){
+                count++;
+        }else{
+            count--;
+        }
+    }
+    return major
+};
