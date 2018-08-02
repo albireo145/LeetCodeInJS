ef1c97e9ec663b691e6c943725a47ca2fecd5962
Albireo <fujishimamao@gmail.com>
2018-08-02 10:50:12 +0800

    modify 606 recursion

--- LeetCodeInJs/606_ConstructStringFromBinaryTree.js
+++ LeetCodeInJs/606_ConstructStringFromBinaryTree.js
@@ -9,25 +9,39 @@
  * @param {TreeNode} t
  * @return {string}
  */
+//  TODO 
+// var tree2str = function(t) {
+//   var arr = [t.val]
+//   var stk = [t]
+//   var str = ''
+//   while(stk.length > 0) {
+//     var curNode = stk[stk.length - 1]
+//     while(curNode.left) {
+//       str += '(' + curNode.left.val
+//       stk.push(curNode.left)
+//       curNode = curNode.left
+//     }
+//     if(curNode.right) {
+//       stk.push(curNode.right)
+//       str += '()'
+//       str += '(' + curNode.right.val
+//     } else {
+//       str += ')'
+//       stk.pop()
+//     }
+//   }
+// };
 var tree2str = function(t) {
-  var arr = [t.val]
-  var stk = [t]
-  var str = ''
-  while(stk.length > 0) {
-    var curNode = stk[stk.length - 1]
-    while(curNode.left) {
-      str += '(' + curNode.left.val
-      stk.push(curNode.left)
-      curNode = curNode.left
-    }
-    str += '()('
-    if(curNode.right) {
-      stk.push(curNode.right)
-      str += curNode.right.val
-      curNode = curNode.rigth
-    } else {
-      str += '))'
-      stk.pop()
-    }
+  if (!t) return ''
+  var str = t.val
+  var left = t.left ? '(' + arguments.callee(t.left) + ')' : ''
+  var right = t.right ? '(' + arguments.callee(t.right) + ')' : ''
+  if (left === '' && right !== '') {
+    return str + '()' + right
   }
-};
+  return str + left + right
+}
+var l2 = {val: 2, left: null, right: null}
+var r3 = {val: 3, left: null, right: null}
+var root = {val: 1, left: l2, right: r3}
+tree2str(root)
